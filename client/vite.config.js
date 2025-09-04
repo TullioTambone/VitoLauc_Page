e2ccff8a903dfaf1674a5ce039cd760f83ff0a9b
TullioTambone <tulliotambone97@gmail.com>
2025-09-04 14:56:03 +0200

    build test

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// vite.config.js
 export default defineConfig({
   plugins: [vue()],
   server: {
