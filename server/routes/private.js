b4919f4c3b2c3586767d4ba93db994a218558b89
TullioTambone <tulliotambone97@gmail.com>
2025-09-04 16:37:44 +0200

    .

---
+++ server/routes/private.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+  res.json({ message: 'Test route funziona!' });
+});
+
+
+module.exports = router;
