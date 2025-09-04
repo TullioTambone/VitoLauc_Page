d12ae50cb2bff6782207bb3bf7601d05a2dca5c6
TullioTambone <tulliotambone97@gmail.com>
2025-09-04 17:04:24 +0200

    cambio linguaggio...

--- server/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const db = require('../db');
-
-exports.findByUsername = (username) => {
-  return db.execute('SELECT * FROM users WHERE username = ?', [username])
-    .then(([rows]) => rows)
-    .catch(err => {
-      throw err;
-    });
-};
